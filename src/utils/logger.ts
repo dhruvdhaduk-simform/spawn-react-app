2f3b1a8b77387afeda0ded2b1afba1d6fa4659f7
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-06 16:18:50 +0530

    Feature/scaffold vite template (#4)
    * refractor: Remove unnecessary console logs from index.ts
    
    * feature: Create function to prompt user about project name and language
    
    * feature: Create a declaration file with UserPrompts interface
    
    * refractor: Move the promptUser function to a seperate file in utils directory
    
    * feature: Add robust error handling
    
    * refractor: Format the vite-react-ts template
    
    * feature: Create utility function scaffoldProject to scaffold project from vite template
    
    * feature: Create .ejs templates for index.html, package.json and README.md
    
    * feature: Create utility functions to render index.html, package.json, README.md
    
    * feature: Render the index.html, package.json, README.md templates from scaffoldProject function
    
    * feature: Create utility functions to log success or error
    
    * feature: Create async main function in index.ts to initialize the cli
    
    * refractor: Put the ejs template logic on one line only
    
    * feature: Add import alias in vite template
    
    * feature: Use the import alias in main.tsx of vite template
    
    * feature: Add console logs to show how to run the project

---
+++ src/utils/logger.ts
@@ -0,0 +1,7 @@
+export function logSuccess(msg: string) {
+    console.log(`\u001b[32m ${msg} \u001b[0m`);
+}
+
+export function logError(msg: string) {
+    console.log(`\u001b[31m ${msg} \u001b[0m`);
+}
---
+++ src/utils/scaffoldProject.ts
@@ -0,0 +1,41 @@
+import type { UserPrompts } from '../interfaces';
+import fs from 'fs';
+import path from 'path';
+import {
+    renderIndexHtml,
+    renderPackageJson,
+    renderReadme,
+} from './renderTemplates';
+import { logSuccess } from './logger';
+
+export function scaffoldProject(userPrompts: UserPrompts): void {
+    const projectDir = path.join(process.cwd(), userPrompts.projectName);
+
+    const viteTemplateDir = path.join(__dirname, '../../vite-react-ts');
+
+    if (fs.existsSync(projectDir)) {
+        throw new Error('The directory you specified already exists.');
+    }
+
+    fs.cpSync(viteTemplateDir, projectDir, { recursive: true });
+
+    fs.writeFileSync(
+        path.join(projectDir, 'index.html'),
+        renderIndexHtml(userPrompts.projectName)
+    );
+    fs.writeFileSync(
+        path.join(projectDir, 'package.json'),
+        renderPackageJson(userPrompts.projectName)
+    );
+    fs.writeFileSync(
+        path.join(projectDir, 'README.md'),
+        renderReadme(userPrompts.projectName)
+    );
+
+    logSuccess(`Initialized your React project at ${projectDir}`);
+    console.log();
+    console.log('  Now run :');
+    console.log(`    cd ${userPrompts.projectName}`);
+    console.log('    npm install');
+    console.log('    npm run dev');
+}
