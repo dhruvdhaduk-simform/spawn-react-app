04f0aa322d8ab34aafb0297f47f38a168d7e7cca
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-06 14:02:55 +0530

    feature: Render the index.html, package.json, README.md templates from scaffoldProject function

--- src/utils/scaffoldProject.ts
+++ src/utils/scaffoldProject.ts
@@ -1,6 +1,11 @@
 import type { UserPrompts } from '../interfaces';
 import fs from 'fs';
 import path from 'path';
+import {
+    renderIndexHtml,
+    renderPackageJson,
+    renderReadme,
+} from './renderTemplates';
 
 export function scaffoldProject(userPrompts: UserPrompts): void {
     const projectDir = path.join(process.cwd(), userPrompts.projectName);
@@ -12,6 +17,19 @@ export function scaffoldProject(userPrompts: UserPrompts): void {
     }
 
     fs.cpSync(viteTemplateDir, projectDir, { recursive: true });
+
+    fs.writeFileSync(
+        path.join(projectDir, 'index.html'),
+        renderIndexHtml(userPrompts.projectName)
+    );
+    fs.writeFileSync(
+        path.join(projectDir, 'package.json'),
+        renderPackageJson(userPrompts.projectName)
+    );
+    fs.writeFileSync(
+        path.join(projectDir, 'README.md'),
+        renderReadme(userPrompts.projectName)
+    );
 
     console.log();
     // console.log(`Initialized your React project at ${projectDir}`);
